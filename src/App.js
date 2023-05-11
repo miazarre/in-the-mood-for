551c53264e62daf08abfdface123eb6f65f20744
miazarre <mia.g.dev@gmail.com>
2023-05-11 16:50:26 +0200

    Initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+export default function App() {
+  return (
+  <div class="bg-gray-900 flex content-center justify-center h-full">
+    <div class="my-20">
+      <h1 class="text-neutral-50 text-5xl font-bold uppercase">
+        In the Mood for
+      </h1>
+    </div>
+  </div>
+  )
+}
