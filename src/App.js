e4e1f30d2df3e19db8b1bac3baa14fadf6c96919
miazarre <mia.g.dev@gmail.com>
2023-05-27 17:03:58 +0200

    Cleaning & adding MIT License

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
-import Home from './components/Home';
 import Footer from './components/Footer';
 import Error from './components/Error';
+import Home from './components/Home';
 import Mood from './components/Mood';
 
 import './index.css';
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import moodsData from '../../data/moods.json';
 
+
 const Home = () => {
   return (
     <>
